b3e936152628bcda33de01d7d5120ccf487fc524
AlvinBrocke <alvin.brocke@ashesi.edu.gh>
2024-04-12 19:09:06 +0000

    updating booking

--- settings/connection.php
+++ settings/connection.php
@@ -1,18 +1,7 @@
 <?php
-// $servername = '127.0.0.1';
-// $username = 'root';
-// $password = 'umy1/KrzQsxA';
-// $database = 'car_rental';
-
-// $conn = new mysqli($servername, $username, $password, $database);
-
-// if ($conn->connect_error) {
-//     die("Connection failed: " . $conn->connect_error);
-// }
-
-$servername = 'localhost';
+$servername = '127.0.0.1';
 $username = 'root';
-$password = '';
+$password = 'umy1/KrzQsxA';
 $database = 'car_rental';
 
 $conn = new mysqli($servername, $username, $password, $database);
